d799f9de247484711ed634cbf4be374ac29cc3fa
Razvan Serban <razvan_tankian@yahoo.com>
2019-08-19 22:12:28 +0300

    Initial commit

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas WelcomeScreen;
+
+    [SerializeField]
+    private Canvas MainMenuScreen;
+
+
+    public void OnClickStartButton()
+    {
+        MainMenuScreen.gameObject.SetActive(true);
+        WelcomeScreen.gameObject.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuScreen.gameObject.SetActive(false);
+        WelcomeScreen.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
