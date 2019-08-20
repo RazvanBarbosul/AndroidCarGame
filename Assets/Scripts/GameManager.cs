3612c0f23daf3565b4dd43d801dd61829633b972
Razvan Serban <razvan_tankian@yahoo.com>
2019-08-20 23:06:39 +0300

    Level Design

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager Instance;
+    private int CurrentLoadedSceneIndex;
+
+    public static GameManager Get()
+    {
+        return Instance;
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+            Debug.Log("GameManager Instance: " + (Instance == this));
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
